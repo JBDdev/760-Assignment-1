26d2c98442cc7b4ae2cba61cfc1adabbbaa05a71
JBDdev <brodydavison4@gmail.com>
2022-09-25 15:33:06 -0400

    Seek Steering scene implemented

---
+++ Assets/Scripts/SeekSteering.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekSteering : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float maxAcceleration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        SteerCharacter();
+    }
+
+    void SteerCharacter() 
+    {
+        //Get Direction to target
+        Vector3 direction = target.position - transform.position;
+        direction.Normalize();
+        transform.GetComponent<Rigidbody2D>().AddForce(direction * maxAcceleration, ForceMode2D.Impulse);
+
+        getOrientation(transform.rotation.z, direction);
+    }
+
+    void getOrientation(float previousOrientation, Vector3 velocity)
+    {
+        transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(-velocity.x, velocity.y) * Mathf.Rad2Deg, Vector3.forward);
+    }
+}
